0d7361184a2cf69fbe05e7f6d2ead56228813813
Lyon <flowingsun007@163.com>
2018-12-25 20:25:22 +0800

    Add files via upload

---
+++ src/main/java/com/flowingsun/article/dto/ArticleThank.java
@@ -0,0 +1,56 @@
+package com.flowingsun.article.dto;
+
+import java.util.Date;
+
+public class ArticleThank {
+
+    private Integer articleId;
+
+    private Integer thankNum;
+
+    private Integer userId;
+
+    private String userName;
+
+    private Date thankDate;
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public Integer getThankNum() {
+        return thankNum;
+    }
+
+    public void setThankNum(Integer thankNum) {
+        this.thankNum = thankNum;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getThankDate() {
+        return thankDate;
+    }
+
+    public void setThankDate(Date thankDate) {
+        this.thankDate = thankDate;
+    }
+}
---
+++ src/main/java/com/flowingsun/article/dto/RegularRecommend.java
@@ -0,0 +1,64 @@
+package com.flowingsun.article.dto;
+
+public class RegularRecommend {
+
+    private Integer articleId;
+
+    private Integer previousArticleId;
+
+    private Integer nextArticleId;
+
+    private String previousTitle;
+
+    private String nextTitle;
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public Integer getPreviousArticleId() {
+        return previousArticleId;
+    }
+
+    public void setPreviousArticleId(Integer previousArticleId) {
+        this.previousArticleId = previousArticleId;
+    }
+
+    public Integer getNextArticleId() {
+        return nextArticleId;
+    }
+
+    public void setNextArticleId(Integer nextArticleId) {
+        this.nextArticleId = nextArticleId;
+    }
+
+    public String getPreviousTitle() {
+        return previousTitle;
+    }
+
+    public void setPreviousTitle(String previousTitle) {
+        this.previousTitle = previousTitle;
+    }
+
+    public String getNextTitle() {
+        return nextTitle;
+    }
+
+    public void setNextTitle(String nextTitle) {
+        this.nextTitle = nextTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "RegularRecommend{" +
+                "previousArticleId=" + previousArticleId +
+                ", nextArticleId=" + nextArticleId +
+                ", previousTitle='" + previousTitle + '\'' +
+                ", nextTitle='" + nextTitle + '\'' +
+                '}';
+    }
+}
