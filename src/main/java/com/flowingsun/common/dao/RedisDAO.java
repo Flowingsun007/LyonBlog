f5a04d329e0289859ff15e679aeef23948413d93
Lyon <769010256@qq.com>
2019-02-07 18:13:30 +0800

    ...

--- src/main/java/com/flowingsun/common/dao/RedisDAO.java
+++ src/main/java/com/flowingsun/common/dao/RedisDAO.java
@@ -193,24 +193,6 @@ public class RedisDAO {
         return result;
     }
 
-    public <T> String setList(String key ,List<T> list,int seconds){
-        String result="setList_fail";
-        try {
-            Jedis jedis = jedisPool.getResource();
-            try{
-                byte[] listInfo = SerializeUtils.serialize(list);
-                result = jedis.set(key.getBytes(), listInfo);
-                //设置键的过期时间为seconds秒
-                jedis.expire(key,seconds);
-            }finally {
-                jedis.close();
-                return result;
-            }
-        } catch (Exception e) {
-            logger.error("Set key error : "+e);
-            return result;
-        }
-    }
 
     public <T> List<T> getList(String key){
         List<T> list = null;
@@ -252,3 +234,4 @@ public class RedisDAO {
         return result;
     }
 }
+
