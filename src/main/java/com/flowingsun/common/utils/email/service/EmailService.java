73c6cc1c19d3192fc1579c8657f734877c51268e
Flowingsun007 <flowingsun007@163.com>
2019-12-25 13:11:50 +0800

    .

--- src/main/java/com/flowingsun/common/utils/email/service/EmailService.java
+++ src/main/java/com/flowingsun/common/utils/email/service/EmailService.java
@@ -12,14 +12,13 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
-//这里用Component注解也可以
 @Service("emailService")
 public class EmailService {
     private static final String HOST = "smtp.163.com";
     private static final Integer PORT = 25;
     private static final Integer ALI_PORT = 465;
     private static final String USERNAME = "flowingsun007@163.com";
-    private static final String PASSWORD = "wy920726zly";
+    private static final String PASSWORD = "xxx";
     private static final String EMAILFORM = "flowingsun007@163.com";
     private static JavaMailSenderImpl mailSender = createAliMailSender();
 
